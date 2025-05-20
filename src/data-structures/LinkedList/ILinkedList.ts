13b99a960574c2b3d57a5507c396b94bc44f30f5
K.Lysov <K.Lysov@russianpost.ru>
2025-05-20 13:46:54 +0300

    доработал связный список

--- src/data-structures/LinkedList/ILinkedList.ts
+++ src/data-structures/LinkedList/ILinkedList.ts
@@ -1,2 +1,26 @@
 /** Структура данных - Связный список */
-export interface ILinkedList {}
+export interface ILinkedList<T> {
+    /** Добавляет элемент в начало списка */
+    prepend(value: T): ILinkedList<T>;
+
+    /** Добавление элемента в конец списка */
+    append(value: T): ILinkedList<T>;
+
+    /** Добавление значения по индексу */
+    insert(value: T, index: number): ILinkedList<T>;
+
+    /** Получить элемент по индексу */
+    getAt(index: number): T | undefined;
+
+    /** Удалить элемент по индексу (возвращает значение удаленного элемента) */
+    removeAt(index: number): T | undefined;
+
+    /** Преобразует список в массив */
+    toArray(): T[];
+
+    /** Возвращает количество элементов в списке */
+    getSize(): number;
+
+    /** Очистить список */
+    clear(): void;
+}
