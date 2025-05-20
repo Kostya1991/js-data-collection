f2a2446aef1fe42d40b581a028e4fe819ba86853
K.Lysov <K.Lysov@russianpost.ru>
2025-05-20 12:37:38 +0300

    поправил сборку vite

--- js-data-collection-docs/vite.config.ts
+++ js-data-collection-docs/vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "js-data-collection",
     build: {
-        outDir: "../docs",
+        outDir: "../dist",
     },
 });
